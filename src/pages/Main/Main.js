e7c05f5d3fda94ef09c0ac01d025bf1388abddcb
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-15 18:19:17 -0800

    begin adding api call to page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
 import '../../css/style.css';
+import { MainForm, MainInput, MainCard, MainFormButton, ImageResult } from './Main.elements';
 
-const Main = () => {
+function Main() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input: input }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    
+  }
+
   return (
   <>
     <Container>
-      <Card>
-        <h1>Hello World!</h1>
-      </Card>
+      <ImageResult src={result} />
+      <MainCard>
+        <MainForm onSubmit={onSubmit}>
+          <MainInput
+            type="text"
+            name="input"
+            placeholder='Enter descriptive text to receive an AI generated image'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <MainFormButton type="submit">Submit</MainFormButton>
+        </MainForm>
+      </MainCard>
     </Container>
   </>
   )
