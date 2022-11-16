3f04d43a27519afc34a193a7f11c46c4165fa4f9
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-15 16:19:53 -0800

    add initial main page setup

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Card } from 'react-bootstrap';
+import '../../css/style.css';
+
+const Main = () => {
+  return (
+  <>
+    <Container>
+      <Card>
+        <h1>Hello World!</h1>
+      </Card>
+    </Container>
+  </>
+  )
+}
+
+export default Main;
