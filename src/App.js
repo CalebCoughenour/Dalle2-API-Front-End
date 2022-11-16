70e6b70ec3be4c6e1e3b26ef4b199994ab493fad
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-15 16:21:54 -0800

    add routes to home page and main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Main from './pages/Main/Main';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/MakeItArt' element={<Main />} />
+        </Routes>
+      </Router>
     </>
   );
 }
