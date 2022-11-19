861540c03ef63496c89e592130a73335a055d3be
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-18 16:30:13 -0800

    remove most code and replace with api call component

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,43 +1,11 @@
 import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
+import PostForm from '../../api/PostForm';
 import '../../css/style.css';
-import { MainForm, MainInput, MainCard, MainFormButton, ImageResult } from './Main.elements';
 
 function Main() {
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState();
-
-  async function onSubmit(event) {
-    event.preventDefault();
-    const response = await fetch("api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ input: input }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    
-  }
-
   return (
   <>
-    <Container>
-      <ImageResult src={result} />
-      <MainCard>
-        <MainForm onSubmit={onSubmit}>
-          <MainInput
-            type="text"
-            name="input"
-            placeholder='Enter descriptive text to receive an AI generated image'
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <MainFormButton type="submit">Submit</MainFormButton>
-        </MainForm>
-      </MainCard>
-    </Container>
+    <PostForm />
   </>
   )
 }
