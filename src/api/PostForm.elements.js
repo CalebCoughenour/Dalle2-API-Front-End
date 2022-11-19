50741e1663e0e6e7740cd271e4791352e6c94a8b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-18 16:33:47 -0800

    move main elements to api call elements

---
+++ src/api/PostForm.elements.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const MainForm = styled.form`
+  margin: 100px;
+`;
+
+export const MainInput = styled.input`
+  width: 90%;
+`;
+
+export const MainCard = styled.div`
+  margin-top: 60vh;
+  padding: 50px;
+  border: 1px solid black;
+`;
+
+export const MainFormButton = styled.button`
+  float: right;
+`;
+
+export const ImageResult = styled.img`
+
+`;
