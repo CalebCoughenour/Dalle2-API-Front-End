ee2589b34b020106802820c0c7de57cdfbc66e82
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-21 22:55:31 -0800

    add styling to page

--- src/api/PostForm.elements.js
+++ src/api/PostForm.elements.js
@@ -1,23 +1,68 @@
 import styled from "styled-components";
 
 export const MainForm = styled.form`
-  margin: 100px;
+  margin: auto;
+  text-align: center;
+  display: block;
 `;
 
-export const MainInput = styled.input`
-  width: 90%;
-`;
-
-export const MainCard = styled.div`
-  margin-top: 60vh;
-  padding: 50px;
+export const FormInput = styled.input`
+  width: 70%;
+  margin: auto;
+  border-radius: 5px;
   border: 1px solid black;
+  padding-left: 10px;
+`;
+
+export const FormCard = styled.div`
+  margin: auto;
+  width: 100%;
+  padding: 50px;
+  border: 2px solid black;
+  border-radius: 10px;
 `;
 
 export const MainFormButton = styled.button`
-  float: right;
+  margin: auto;
+  margin-top: 20px;
+  background-color: transparent;
+  width: 30%;
+  height: 35px;
+  border: solid 1px black;
+
+  &:hover {
+    color: #61b38f;
+    background-color: #242422;
+    transition: background-color .4s ease-in-out;
+  }  
+`;
+
+export const ImgWrapper = styled.div`
+  margin: auto;
+  padding: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  text-align: center;
 `;
 
 export const ImageResult = styled.img`
+  margin: auto;
+  width: 400px;
+  border-radius: 5px;
+  border: 2px solid black;
 
+  @media screen and (max-width: 1200px){
+    width: 80%;
+  }
+`;
+
+
+export const FormSelect = styled.select`
+  margin: auto;
+  border-radius: 5px;
+`;
+
+export const SelectOption = styled.option`
+  text-align: center;
 `;
