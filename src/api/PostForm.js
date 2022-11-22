4a1a2157949b7de4dd5cb2d4ac426a961faefc0f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-21 22:55:06 -0800

    add input for size, error handling and conditional image for initial page load

--- src/api/PostForm.js
+++ src/api/PostForm.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { MainForm, MainInput, MainCard, MainFormButton, ImageResult } from './PostForm.elements';
+import { MainForm, FormInput, FormCard, MainFormButton, ImageResult, ImgWrapper, FormSelect, SelectOption } from './PostForm.elements';
+import Container from 'react-bootstrap/Container';
+import './api-styles.css';
 
 
 export default function PostForm() {
-  // useState will be called when the form is entered and when the API response changes the image
+  // useState will be called when the form is entered
   const [prompt, setPrompt] = useState(' ');
-  // const [n, setN] = useState(1);
-  // const [size, setSize] = useState('512x512');
+  const [size, setSize] = useState('Size not entered');
+  // image is null until respone from API
   const [image, setImage] = useState(null);
 
   // Function containing the API call
@@ -16,11 +18,11 @@ export default function PostForm() {
     const config = {
       headers: { Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}` }
     };
-    // Body for API call
+    // Body for API call; prompt & size get change with set state when form is entered
     const body = {
       prompt: prompt,
       n: 1,
-      size: "1024x1024"
+      size: size
     }
     // Axios API call to openai image generated, takes in body and bearer token(config)
     await axios.post('https://api.openai.com/v1/images/generations', body, config)
@@ -31,32 +33,54 @@ export default function PostForm() {
     .catch(function(error){
       // Display error if there is a problem with the call
       console.log(error);
+      alert("There was a problem with the call, please try again or change your prompt");
     });
   }
   
   // On form submit, call useGetImage function
   function handleSubmit(e) {
     e.preventDefault();
+    //trim prompt to check if empty string was entered
+    const imagePrompt = prompt.trim();
+    if (imagePrompt === ''){
+      alert("Please enter a prompt");
+      return;
+    // check if size is set
+    } else if (size === 'Select A Size'){
+      alert("Please select a size");
+      return;
+    }
     useGetImage();
   };
   
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Enter A Prompt</label>
-          <input 
-          type="text"
-          name="prompt"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type='submit'>Submit</button>
-        </div>
-      </form>
-      <img src={image} />
+      <Container>
+        <MainForm onSubmit={handleSubmit}>
+          <FormCard>
+            <FormInput 
+            type="text"
+            name="prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="ex: Cat's having a tea party in space, 4k, high quality, realistic"
+            />
+            <FormSelect 
+            name="size"
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            >
+              <SelectOption value="Select A Size" onChange={(e) => setSize(e.target.value)}>Select A Size</SelectOption>
+              <SelectOption value='512x512' onChange={(e) => setSize(e.target.value)}>512x512</SelectOption>
+              <SelectOption value='1024x1024' onChange={(e) => setSize(e.target.value)}>1024x1024</SelectOption>
+            </FormSelect>
+            <MainFormButton className='form-button' type='submit'>Submit</MainFormButton>
+          </FormCard>
+        </MainForm>
+        <ImgWrapper>
+          {image === null ? <ImageResult src='images/y00t/yoot9040.webp' alt='Y00ts NFT #9040'/> : <ImageResult src={image} /> }
+        </ImgWrapper>
+      </Container>
     </div>
   )
 }
