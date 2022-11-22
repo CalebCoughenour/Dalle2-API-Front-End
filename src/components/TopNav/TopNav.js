e1a9449f6b254f18ef12df7307b73dea876c5154
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-21 22:50:32 -0800

    add nav bar to with title linking to twitter

---
+++ src/components/TopNav/TopNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavWrapper, NavTitle } from './TopNav.elements';
+import './nav-styles.css';
+
+export default function TopNav() {
+  return(
+    <>
+      <NavWrapper>
+        <NavTitle className='nav-title' href="https://twitter.com/makeitartai" target="_blank" rel="noopener noreferrer">MAKE IT ART</NavTitle>
+      </NavWrapper>
+    </>
+  )
+};
