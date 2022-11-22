5e349485f3d21cc4e3fc8c5918f5b220eaf06f55
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-21 22:50:55 -0800

    add styling for topnav

---
+++ src/components/TopNav/TopNav.elements.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const NavWrapper = styled.div`
+  padding-top: 15px;
+  padding-bottom: 5px;
+  text-align: center;
+`;
+
+export const NavTitle = styled.h1`
+  margin: auto;
+  width: 18%;
+  font-family: 'Megrim', cursive;
+  color: black;
+  font-size: 30px;
+`;
