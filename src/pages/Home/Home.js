ce0b41314e03380747a17f3e9ef1c3757324b496
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-11-21 22:47:04 -0800

    add anchor tag to yoot icon image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { HomeBody, HomeButton, HomeTitle, HomeLink, HomeCard, HomeImage } from './Home.elements';
+import { HomeBody, HomeButton, HomeTitle, HomeLink, HomeCard, HomeImage} from './Home.elements';
 import '../../css/style.css';
 import './home-styles.css';
 
@@ -12,12 +12,14 @@ const Home = () => {
     <HomeBody className="color-primary-bg">
       <Container>
         <HomeCard className="home-card">
-          <HomeTitle className='home-title'>Make It<br/> Art</HomeTitle>
+          <HomeTitle>Make It<br/> Art</HomeTitle>
           <HomeLink href="/MakeItArt"><HomeButton>ENTER</HomeButton></HomeLink>
         </HomeCard>
         <Row style={{ textAlign: 'center', marginBottom: '5vh' }}>
           <Col>
+          <HomeLink href="https://twitter.com/makeitartai" target="_blank" rel="noopener noreferrer">
             <HomeImage src='images/y00t/Yoot9040.webp' alt='Y00ts NFT #9040'/>
+          </HomeLink>
           </Col>
         </Row>
       </Container>
